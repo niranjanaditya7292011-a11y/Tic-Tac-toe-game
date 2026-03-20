5c9e1bb7183ff7a2ff71b56120229eb9690deef9
niranjanaditya7292011-a11y <niranjan.aditya7292011@gmail.com>
2026-03-20 20:30:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
+// Serve frontend files
+app.use(express.static(path.join(__dirname)));
+
+// When someone opens site
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Socket connection
 io.on("connection", (socket) => {
   console.log("User Connected");
 
@@ -11,4 +22,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => console.log("Server running"));
+// IMPORTANT for Render
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
